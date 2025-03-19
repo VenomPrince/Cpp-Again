3bc5aa7a4da07220a51b74dd3bf79d82ca16f404
VenomPrince <princekhand09@gmail.com>
2025-03-19 12:41:57 +0000

    Test 3

--- Helloworld.cpp
+++ Helloworld.cpp
@@ -1,10 +1,18 @@
+// These are heades
 #include <stdio.h>
 #include <iostream>
+// Comment using /* */ 
 int main(){
+    // Using namespace std here so we don't have to write std::cout, std::endl, etc.
     using namespace std;
+    // argc is the number of arguments passed to the program
     int argc = 1;
-    printf("Hello world\n");
-    cout << "Hello world" << "\n" << endl;
+    // printf is a C function, cout is a C++ object
+    printf("Hello world!\n");
+    // cout is an object of ostream class
+    cout << "Hello world!" << "\n" << endl;
+    // argc is the variable name, argv is the array of arguments
     cout << "Number of arguments: " << argc << endl;
-    return 0;
+    // argv[0] is the name of the program
+    return 0; // Return 0 to indicate that the program has run successfully
 }
