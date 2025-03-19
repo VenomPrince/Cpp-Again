6eeaec828eb8795e9b89aaffdf86347a84ffd7d4
VenomPrince <princekhand09@gmail.com>
2025-03-19 12:36:19 +0000

    Test 2

--- Helloworld.cpp
+++ Helloworld.cpp
@@ -1,5 +1,10 @@
 #include <stdio.h>
+#include <iostream>
 int main(){
+    using namespace std;
+    int argc = 1;
     printf("Hello world\n");
+    cout << "Hello world" << "\n" << endl;
+    cout << "Number of arguments: " << argc << endl;
     return 0;
 }
